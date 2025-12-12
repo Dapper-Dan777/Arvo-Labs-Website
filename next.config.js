6cde558a92d35259a8aeb65fbf8f67fae45f677a
Adrian <adrianthome@3aa04337-b47c-4c75-b76a-1aa89e7633b7.fritz.box>
2025-12-12 17:06:19 +0100

    Disable ESLint during Vercel builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // Deaktiviert, um doppeltes Rendering zu vermeiden
+  eslint: {
+    // Linting darf lokal laufen, aber nicht den Vercel-Build abbrechen
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
